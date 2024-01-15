23fbc79b795708cdf8a6a6e5833270449279fa02
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2024-01-15 20:32:19 +0430

    add react components for greating

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,8 +1,8 @@
 class GreetingsController < ApplicationController
   def index
-    @random_message = Greeting.order('RANDOM()').first
+    @random_message = Message.order('RANDOM()').first
     if @random_message
-      render json: { greeting: @random_message.greeting }
+      render json: { greeting: @random_message.content }
     else
       render json: { error: 'No greetings available' }, status: :not_found
     end
